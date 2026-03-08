768b501234b4d48a501253e7a158da502e55d434
divineexcel4 <godwindivine566@gmail.com>
2026-03-08 17:21:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// function showMessage() {
+//     alert("Button clicked! javascript is working");    
+// }
+
+
+// function changeText() {
+//     document .getElentById("text") .innerHTML = "text changed successfully!";
+// }
+
+// let menu = document.getElementById("menu");
+// let list = document.createElement("li");
+// list.innerHTML = "contact";
+// menu.appendChild(list);
+
+// text content
+
+// let menu = document.getElementById("menu");
+// console.log(menu.innerText);
+// console.log(Textcontent)
+
+// #attribute method
+
+// let input = document.getElementById("username");
+// console.log(inputBox.attributes);
+// console.log(input.getAttribute("placeholder"))
+
+// inputBox.setAttribute("class", "user");
+// console.log(inputBox)   
+
+// console.log(inputBOX.style);
+
+// inputBox.style.padding = "10px";
+// inputBox.style.cssText += "width:200px; height:100px";
+// console.log(inputBox);
+
+// window.getComputedStyle(Element, pseudoElement)
+// console.log(window.getComputedStyle(inputBox));
+
+// let title = document.getElementById("title")
+// // title.className += "new";
+// console.log(title);
+// console.log(title.className)
+
+// title.classList.add("new", "new2");
+// title.classList.remove("message", "main");
+
+// console.log(title.classList);
+
+
+// #event
+// function displaymsg(){
+//     console.log("Button clicked from fn");
+// }
+
+let btn = document.getElementById("btn")
+// btn.onclick = function(){
+//     console.log("button clicked")
+// }
+
+// #eventlistener
+addEventListener('click', function(){
+    console.log("button clicked");
+})
+
+
